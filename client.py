0f770c204e059ef10c9bcdf1a956ec186750edd2
nikichishna <78732951+nikichishna@users.noreply.github.com>
2021-05-28 13:48:01 +0700

    Update client.py

--- client.py
+++ client.py
@@ -2,12 +2,7 @@ import requests
 r = requests.get('http://localhost:5000/')
 print(r.status_code)
 print(r.text)
-Изменим файл st.sh на следующий:
-gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
-sleep 5
-echo start client
-python3 client.py
-sleep 5
-echo $APP_PID
-kill -TERM $APP_PID
-exit 0 
+r = requests.get('http://localhost:5000/data_to') 
+80
+print(r.status_code)
+print(r.text)
